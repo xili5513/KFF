aacfbdb1472b52a0bdbc8b39f28b29cb278a37bc
彭嘉润 <pengjiarun@vlan-2638-10-19-61-111.student.wireless.sydney.edu.au>
2019-04-16 15:51:34 +1000

    application

--- foodfinder/tutorial/snippets/urls.py
+++ foodfinder/tutorial/snippets/urls.py
@@ -4,11 +4,11 @@ from snippets import views
 from django.conf.urls import include
 
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/data', views.SnippetList.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
-    path('snippets/home', views.home, name='home'),
+    path('snippets/', views.home, name='home'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
